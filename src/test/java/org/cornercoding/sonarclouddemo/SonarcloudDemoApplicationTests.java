b139454d40e5d644be99ebe8a89c8a1a9835d925
Brunomykael <167835155+Brunomykael@users.noreply.github.com>
2024-06-24 22:11:09 -0300

    Update SonarcloudDemoApplicationTests.java

--- src/test/java/org/cornercoding/sonarclouddemo/SonarcloudDemoApplicationTests.java
+++ src/test/java/org/cornercoding/sonarclouddemo/SonarcloudDemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SonarcloudDemoApplicationTests {
-	pirocão
+	pirocao
 	@Test
 	public void contextLoads() {
 	}
